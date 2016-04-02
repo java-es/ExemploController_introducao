07a89427615e514371ee4417c7208c258fa065f6
Wanderson Alves Rodrigues <wandersonallves@yahoo.com.br>
2016-04-02 17:41:39 -0300

    Criação do projeto de introdução a controller ASP.NET MVC.

---
+++ ExemploController/ExemploController/Controllers/CalculadorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace ExemploController.Controllers
+{
+    public class CalculadorController : Controller
+    {
+        public ActionResult Somar()
+        {
+            return Content("Ação de Somar");
+        }
+
+        public ActionResult Subtrair()
+        {
+            return Content("Ação de Subtrair");
+        }
+
+        public ActionResult Multiplicar()
+        {
+            return Content("Ação de Multiplicar");
+        }
+
+        public ActionResult Dividir()
+        {
+            return Content("Ação de Dividir");
+        }
+    }
+}
